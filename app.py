4d3ee93202d530560e54e277ff2c2fd3467932f2
Lance798 <32232417+Lance798@users.noreply.github.com>
2024-09-18 21:21:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ sbdb_api_url = "https://ssd-api.jpl.nasa.gov/sbdb_query.api"
 # available SBDB Orbit Classes: https://ssd-api.jpl.nasa.gov/doc/sbdb_filter.html
 @app.route("/api/query/<string:code>")
 @app.route("/api/query")
-def query(code="COM"):
+def query(code="MBA"):
     result = requests.get(sbdb_api_url, data={"sb-class": code,
                                                "fields": "e,a,i,om,w,ma"})
     
@@ -19,5 +19,5 @@ def query(code="COM"):
 def hello():
     return "Hello, World!"
 
-if __name__ == 'main':
+if __name__ == '__main__':
     app.run()
